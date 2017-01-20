d2aa738f6b962c68e50984b7251bef8bfc18bf2d
JacobWay <soarinblue@gmail.com>
2017-01-20 13:51:32 +0800

    change footer text

--- js/self.js
+++ js/self.js
@@ -1,26 +1,36 @@
-function toggleMenu(){
-    $("#logo").hover(
-        function(){
-            $(".float-menu").css("visibility", "visible");
-        },
-        function(){
-            $(".float-menu").css("visibility", "hidden");
-        }
-            );
-}
+(function($){
+    function toggleMenu(){
+        $("#logo").hover(
+            function(){
+                $(".float-menu").css("visibility", "visible");
+            },
+            function(){
+                $(".float-menu").css("visibility", "hidden");
+            }
+                );
+    }
 
-function toggleFloat(){
-    $(".float-menu").hover(
-        function(){
-            $(".float-menu").css("visibility", "visible");
-        },
-        function(){
-            $(".float-menu").css("visibility", "hidden");
-        }
-            );
-}
+    function toggleFloat(){
+        $(".float-menu").hover(
+            function(){
+                $(".float-menu").css("visibility", "visible");
+            },
+            function(){
+                $(".float-menu").css("visibility", "hidden");
+            }
+                );
+    }
 
-$(document).ready(function(){
-    toggleMenu();
-    toggleFloat();
-});
+    function addFooterText(){
+        var text = $(".addFooterText").text();
+        var year = new Date().getFullYear();
+        text = "&nbsp;&nbsp;" + year + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + text;
+        $(".addFooterText").html(text);
+    }
+
+    $(document).ready(function(){
+        toggleMenu();
+        toggleFloat();
+        addFooterText();
+    });
+})(window.jQuery)
